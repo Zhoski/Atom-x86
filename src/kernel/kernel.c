f29ea1d0aeb715f4922d6c79ad7da03b7eabc030
Zhoski <alekkseev.a@main.ru>
2026-04-25 22:41:47 +0500

    Теперь в ядре есть функции outb inb

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,12 +1,24 @@
 #include "../drivers/vga.h"
 #include <stdint.h>
 
+inline void outb(uint16_t port, uint8_t data) {
+    asm("out %0, %1" :: "a"(port), "d"(data));
+}
+
+inline uint32_t in(uint16_t port) {
+    uint32_t data;
+    asm volatile ("inl %1, %0" : "=a"(data) : "d"(port));
+    return data;
+}
+
 void kmain() {
 	uint8_t* vga_video = (uint8_t*)0xB8000;
-	vga_video[0] = '!';
-	vga_video[1] = 0x07;
+	
 	clear_screen();			// Очистка
-	putchar('A');	
-    	kwrite_string("Zvor");
+	
+    putchar('A');	
+    
+    vga_set_attribute(VGA_COLOR_BLUE, VGA_COLOR_WHITE);
+    kwrite_string("Zvor");
 	for(;;);
 }
