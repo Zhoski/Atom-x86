7cbfc95225d0d42eb9375fb2a23834b681bf2779
Zhoski <alekkseev.a@main.ru>
2026-04-26 16:20:08 +0500

    Теперь корректно обрабатывает нажатия

--- src/drivers/keyboard.c
+++ src/drivers/keyboard.c
@@ -1,3 +1,21 @@
+#include "vga.h"
+
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t data;
+    asm volatile("inb %1, %0" : "=a"(data) : "Nd"(port));
+    return data;
+}
+
 void keyboard_handler() {
+    uint8_t scancode = inb(0x60);
+    if(scancode & 0x80) {
+        return;
+    }
+
+    putchar('!');
     
 }
