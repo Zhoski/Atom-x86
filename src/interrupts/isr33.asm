57270afec39fd5f04f3d7cb26a00a930ca051661
Zhoski <alekkseev.a@main.ru>
2026-04-26 15:33:10 +0500

    Добавлено прерывание клавиатуры

---
+++ src/interrupts/isr33.asm
@@ -0,0 +1,27 @@
+extern keyboard_handler
+
+isr33:
+    cli
+
+    pushad
+    push ds
+    push es
+    push fs
+    push gs
+
+    mov ax, 0x10
+    mov es, ax
+    mov ds, ax
+
+    call keyboard_handler
+
+    mov al, 0x20
+    out 0x20, al
+
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popad
+
+    iretd
