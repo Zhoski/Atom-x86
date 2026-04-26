979a8654b84de88c2844a61b091d492dce10e67c
Zhoski <alekkseev.a@main.ru>
2026-04-26 16:36:23 +0500

    Теперь выводит нажатый символ на экран

--- src/drivers/keyboard.c
+++ src/drivers/keyboard.c
@@ -1,4 +1,7 @@
 #include "vga.h"
+#include "keyboard.h"
+
+uint8_t last_key = 0;
 
 static inline void outb(uint16_t port, uint8_t data) {
     asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
@@ -10,12 +13,18 @@ static inline uint8_t inb(uint16_t port) {
     return data;
 }
 
+uint8_t get_key() {
+    return last_key;
+}
+
 void keyboard_handler() {
     uint8_t scancode = inb(0x60);
     if(scancode & 0x80) {
         return;
     }
-
-    putchar('!');
+    uint8_t c = ascii_table[scancode];
+    if(c != '\n' && c != 0 && c != '\t' && c != '\b') {
+        putchar(c);
+    } 
     
 }
