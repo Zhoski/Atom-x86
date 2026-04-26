492bcb7e3bbbbea0731e9926cb42ff5ec2ade464
Zhoski <alekkseev.a@main.ru>
2026-04-26 15:54:40 +0500

    Драйвер клавиатур

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,8 +3,11 @@
 #include "../cpu/PIC.h"
 #include <stdint.h>
 
+extern void isr33();
+
 void kmain() {
     idt_load();                     // Загрузить IDT
+    idt_set(33, 0x08, 0x8E, (uint32_t)isr33);                                   
     pic_remap();                    // Установка PIC
     pic_irq_mask(0x21, 0b11111101); // Включить только IRQ1
     asm("sti" :: );                 // Включить перывания
