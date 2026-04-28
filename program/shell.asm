0526c808e1481f2a3acfd0ce1aacb79603168e19
Zhoski <alekkseev.a@main.ru>
2026-04-28 23:45:22 +0500

    Теперь вызывает int 0x80

--- program/shell.asm
+++ program/shell.asm
@@ -3,7 +3,13 @@ bits 32
 global shell
 shell:
     mov word [0xB8000], 0x4F41  ; 41 = 'A', 4F
+
+    mov eax, 0    ; Номер системного вызова
+    mov ebx, 0    ; Аргумент 1
+    mov ecx, 0    ; Аргумент 2
+    mov edx, 0    ; Аргумент 3
     
+    int 0x80 
     jmp $
 
     ret
