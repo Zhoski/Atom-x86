80bbebf310eed9083aeffcd44a29a5862eebb6b9
Zhoski <alekkseev.a@main.ru>
2026-04-28 23:47:45 +0500

    Добавлен isr80

--- src/interrupts/isr80.asm
+++ src/interrupts/isr80.asm
@@ -1,4 +1,33 @@
+extern syscall_handler
 global isr80
 
-isr 80:
+isr80: 
+    cli
 
+    pushad
+    push ds
+    push es
+    push fs
+    push gs
+
+    mov ax, 0x10
+    mov es, ax
+    mov ds, ax
+    
+    push edx
+    push ecx
+    push ebx
+    push eax
+
+    call syscall_handler
+
+    add esp, 16
+
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popad
+    
+    sti
+    iret
