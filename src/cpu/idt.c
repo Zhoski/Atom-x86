6827fd72053130b9e1502afde50cd7b0f3e5c82f
Zhoski <alekkseev.a@main.ru>
2026-04-28 23:48:17 +0500

    Незначительное изменение

--- src/cpu/idt.c
+++ src/cpu/idt.c
@@ -3,9 +3,9 @@
 struct InterruptDescriptor32 IDT[256]; // Таблица на 256 прерываний
 
 void idt_load() {
-   struct IDT_pointer idt_ptr;
-   idt_ptr.limit = (uint16_t)(sizeof(struct InterruptDescriptor32) * 256) - 1;
-   idt_ptr.base  = (uint32_t)&IDT;
+    struct IDT_pointer idt_ptr;
+    idt_ptr.limit = (uint16_t)(sizeof(struct InterruptDescriptor32) * 256) - 1;
+    idt_ptr.base  = (uint32_t)&IDT;
 
     asm volatile("lidt (%0)" : : "r"(&idt_ptr));
 }
