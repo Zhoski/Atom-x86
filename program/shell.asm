104bc1037a25ea28ea1d764c23693c7ac4adfe3a
Zhoski <alekkseev.a@main.ru>
2026-05-03 14:34:28 +0500

    Сей перед добавлением цикла

--- program/shell.asm
+++ program/shell.asm
@@ -26,32 +26,247 @@ shell:
 
     call get_user_name
 
-    mov ecx, user_name
-    call print_string
+    ;mov ecx, user_name
+    ;call print_string
 
-    mov ecx, prompt
-    call print_string
+    ;mov ecx, prompt
+    ;call print_string
 
     ;jmp $
 
     ;mov eax, 60
     ;int 0x80 
 
-    jmp loop
-
-    jmp $
+    jmp _shell
 
     ret
 
-loop:
+_shell:
+    call reset_buffer
+
+    mov ecx, user_name
+    call print_string
+
+    mov ecx, prompt
+    call print_string
+
+    mov esi, command_buffer
+    xor edx, edx
+
+.shell_loop:
     mov eax, 2
     mov ebx, 1
     int 0x80
+    
+    cmp eax, 0
+    jz .skip
+
+    cmp eax, 0x0A
+    jz .enter
+
+    mov [esi], eax
+    inc esi
+    inc edx
+
+    pushad
     mov ecx, eax
     mov eax, 1
     mov ebx, 3
     int 0x80
-    jmp loop
+    popad
+
+.skip:
+    jmp .shell_loop
+
+.enter: 
+    jmp execute
+
+parser:
+    mov esi, command_buffer
+
+.parser_loop:
+    cmp byte [esi], 0
+    jz .end
+
+    cmp byte [esi], ' '
+    jnz .skip
+    
+    mov byte [esi], 0
+
+.skip:
+    inc esi
+    jmp .parser_loop
+
+.end:
+    ret
+
+find_next_arg:
+    xor edx, edx 
+.find_loop:
+    ;mov eax, 1
+    ;mov ebx, 3
+    ;mov ecx, [esi]
+    ;int 0x80
+
+    inc esi
+    inc edx
+    cmp byte[esi], 0  
+    jnz .find_loop 
+
+    ret
+
+execute: 
+    call parser
+
+    mov esi, command_buffer
+    mov edi, help
+    call compare_strings
+    je .do_help
+
+    mov esi, command_buffer
+    mov edi, memread
+    call compare_strings
+    je .do_memread
+
+    mov ecx, new_string
+    call print_string
+
+    jmp _shell
+    
+
+.do_help:
+    mov ecx, list
+    call print_string
+    jmp _shell
+
+.do_memread:
+    mov ecx, new_string
+    call print_string
+
+    mov esi, command_buffer
+    call find_next_arg      ; Найти аргумент, в этом случаи адрес
+    
+    inc esi
+
+    push esi
+    call hex_string_to_int 
+    pop esi
+
+    mov ecx, eax
+
+;.while:
+    ;mov eax, 1
+    ;mov ebx, 2
+    ;int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+
+    mov ecx, eax
+    mov eax, 1
+    mov ebx, 4
+    int 0x80
+
+    ;jmp .while
+
+;.end:
+    jmp _shell
+
+; Вход: EAX = 1002 (десятичное)
+; Выход: EAX = 0x1002 (шестнадцатеричное значение)
+hex_string_to_int:
+    xor eax, eax        ; Обнуляем результат
+
+.loop:
+    movzx ecx, byte [esi] ; Берем символ
+    inc esi               ; Сразу двигаем указатель на следующий
+    
+    test cl, cl           ; Конец строки?
+    jz .done
+
+    ; Переводим символ в значение
+    cmp cl, '0'
+    jl .done              ; Мусор (меньше '0')
+    cmp cl, '9'
+    jbe .is_digit         ; Это цифра 0-9
+
+    ; Если не цифра, проверяем на буквы A-F
+    and cl, 0xDF          ; Делаем букву заглавной (маленькая 'a' станет 'A')
+    cmp cl, 'A'
+    jl .done              ; Мусор между '9' и 'A'
+    cmp cl, 'F'
+    jg .done              ; Мусор больше 'F'
+
+    sub cl, 'A' - 10      ; 'A' -> 10, 'B' -> 11 и т.д.
+    jmp .add_to_res
+
+.is_digit:
+    sub cl, '0'           ; '0' -> 0, '1' -> 1
+
+.add_to_res:
+    shl eax, 4            ; Умножаем текущий результат на 16 (сдвиг влево на 4 бита)
+    or  eax, ecx          ; Добавляем новую цифру в младшие 4 бита
+    jmp .loop
+
+.done:
+    ret
+string_to_int:
+    xor eax, eax
+    xor ecx, ecx
+
+.loop:
+    mov cl, [esi]
+    cmp cl, 0
+    jz .done
+
+    cmp cl, '0'
+    jl .done        
+    cmp cl, '9'
+    jg .done         
+
+    sub cl, '0'       
+    
+    imul eax, 10      
+    
+    add eax, ecx       
+    inc esi            
+    jmp .loop
+
+.done:
+    ret
+
+reset_buffer:
+    mov edi, command_buffer ; Сюда буффер   
+    xor al, al              ; al в 0
+    mov cx, 64              ; Сколько байтов обнуляить 
+    cld                     ; CF в 0
+    rep stosb           
+    mov edi, command_buffer  
+    ret
+
+compare_strings:
+    pushad
+    xor ecx, ecx
+
+.next_char:
+    lodsb
+    cmp al, [edi]
+    jne .not_equal
+
+    cmp al, 0
+    je .equal
+
+    inc edi
+    jmp .next_char
+
+.not_equal:
+    popad
+    ret
+
+.equal:
+    popad
+    ret
 
 get_user_name:
     mov eax, 4
@@ -96,5 +311,21 @@ logo2:  db "/_/  |_\__/\____/_/ /_/ /_/     /_/|_/____/\____/ ",10,10,10,0
 help_msg: db "Type 'help' to get a list of commands.",10,0
 prompt: db "$> ",0
 
+; Список команд
+list:   db 10, "+-----------------------------------------------+",10
+        db     "| help - out list of commands                   |",10
+        db     "| clear - clear screen                          |",10
+        db     "+-----------------------------------------------+",10,0
+
+; Команды
+help: db "help",0
+memread: db "memread",0
+
+; Переменные
+count: db 0
+
 ; Буфферы
 user_name: times 32 db 0
+user_pass: times 32 db 0
+command_buffer: times 64 db 0
+hex_buffer: times 16 db 0
