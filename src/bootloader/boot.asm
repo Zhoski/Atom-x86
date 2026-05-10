ae900a2c1ec1966b38ac7db5cce471673524ce83
Zhoski <alekkseev.a@main.ru>
2026-05-10 14:18:16 +0500

    Добавление FAT16

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,6 +1,33 @@
 bits 16
 org 0x7C00
 
+jmp short start         ; Прыжок в старт
+nop
+db "ATOM.FS1"           ; Название
+
+BPB: 
+    dw 512              ; Байт в секторе
+    db 4                ; Секторов в кластере
+    dw 8                ; Зарезервировано
+    db 2                ; FAT будет 2
+    dw 512              ; 
+    dw 0                ; HDA 1.44M
+    db 0xF8             ; Жетский диск
+    dw 256              ; Занято секторов
+    dw 63               ; BPB_SecPerTrk
+    dw 255              ; BPB_NumHeads
+    dd 0
+    dd 204800
+
+EXTENDEF_BPB:
+    db 0x80             ; HDD
+    db 0x00             ; Зарезервировано
+    db 0x29             ; Сигнатура
+    dd 0x12             ; ID тома
+    db "NO NAME    "     
+    db "FAT16   "   
+
+
 start:
     ; Сегменты
     xor ax, ax
@@ -42,6 +69,8 @@ stage2_load_error:
     call print_string
     jmp $
 
+find_file:
+    
 
 print_string:
     pusha
@@ -60,6 +89,7 @@ exit:
 message: db "Booting ",0
 error_msg: db "[ FAIL ]",13,10,0
 ok_msg: db "[ OK ]",13,10,0
+stage2_file: db "stage2.bin",0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
