4403733a8f2c8e09ae11a020f40248180ba96b37
Zhoski <alekkseev.a@main.ru>
2026-05-11 20:59:05 +0500

    Теперь загружает файл загрузчика, а не сектор

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -13,29 +13,29 @@ org 0x7C00
 ; Перепрыгивание BPB
 jmp short start
 nop
-db "ATOM-x86"           ; OEM identifier
+db "ATOM-X86"           ; OEM identifier
 
-; ========= BPB =========
-dw 512                  ; 512 байт на сектор
-db 4                    ; 4 сектора на кластер
-dw 8                    ; Зарезервировано под загрузчик
-db 2                    ; Будет две записи FAT
-dw 256                  ; Записей в root каталоге
-dw 32768                ; 32768 сектора (16Мб)
-db 0xF8                 ; Жетский диск
-dw 128                  ; Размер одной FAT
-dw 63                   ; Секторов на одной дорожке
-dw 255                  ; Головок на одной стороне
-dd 0
-dd 0
+; ================= BPB =================
+BPB_BytsPerSec: dw 512                  ; 512 байт на сектор
+BPB_SecPerClus: db 8                    ; 4 сектора на кластер
+BPB_RsvdSecCnt: dw 1                    ; Зарезервировано под загрузчик
+BPB_NumFATs:    db 2                    ; Будет две записи FAT
+BPB_RootEntCnt: dw 256                  ; Записей в root каталоге
+BPB_TotSec16:   dw 32768                ; 32768 сектора (16Мб)
+BPB_Media:      db 0xF8                 ; Жетский диск
+BPB_FATSz16:    dw 64                   ; Размер одной FAT
+BPB_SecPerTrk:  dw 63                   ; Секторов на одной дорожке
+BPB_NumHeads:   dw 255                  ; Головок на одной стороне
+BPB_HiddSec:    dd 0
+BPB_TotSec32:   dd 0
 
-; ===== Extended BPB =====
-db 0x80                 ; Номер диска
-db 0x00                 ; Зарезервировано
-db 0x29                 ; Сигнатура
-dd "ATOM"               ; Серийный номер
-db "NO NAME    "        ; 
-db "FAT16   "           ; Системный идентификатор
+; =========== Extended BPB ==============
+BS_DrvNum:      db 0x80                 ; Номер диска
+BS_Reserved1:   db 0x00                 ; Зарезервировано
+BS_BootSig:     db 0x29                 ; Сигнатура
+BS_VolID:       dd 0x41544F4D           ; Серийный номер 'ATOM'
+BS_VolLab:      db "NO NAME    "        ; 
+BS_FilSysType:  db "FAT16   "           ; Системный идентификатор
 
 start:
     ; Сегменты
@@ -49,37 +49,31 @@ start:
     mov al, 0x3
     int 0x10
 
-    mov si, message
-    call print_string
+    call ADD_SIGNATURE_FAT
+    call LOAD_BOOT
 
-    xor ax, ax
-    mov es, ax
+    ;xor ax, ax
+    ;mov es, ax
 
     ; Загрузка большого загрузчика по 0x0000:0x8000 из 2 сектора
-    mov ah, 0x2
-    mov al, 6
-    mov cl, 2
-    mov ch, 0
-    mov dh, 0
-    mov dl, 0x80
-    mov bx, 0x8000
-    int 0x13
+    ;mov ah, 0x2
+    ;mov al, 6
+    ;mov cl, 2
+    ;mov ch, 0
+    ;mov dh, 0
+    ;mov dl, 0x80
+    ;mov bx, 0x8000
+    ;int 0x13
 
-    jc stage2_load_error
-
-    mov si, ok_msg
-    call print_string
+    ;jc stage2_load_error
 
     ; Запуск большого загрузчика
-    jmp 0x0000:0x8000
+    ;jmp 0x0000:0x8000
 
 stage2_load_error:
-    mov si, error_msg
+    mov si, not_found 
     call print_string
     jmp $
-
-find_file:
-    
 
 print_string:
     pusha
@@ -95,11 +89,197 @@ exit:
     popa
     ret
 
-message: db "Booting ",0
-error_msg: db "[ FAIL ]",13,10,0
-ok_msg: db "[ OK ]",13,10,0
-stage2_file: db "STAGE2 BIN",0
-root: dw 520                        ; Тут начинается root с учетом резрва в 8 секторов
+ADD_SIGNATURE_FAT:
+    pusha
+    
+    xor ax, ax
+    mov es, ax
+
+    mov ah, 0x2
+    mov al, 6
+    mov cl, 2
+    mov ch, 0
+    mov dh, 0
+    mov dl, 0x80
+    mov bx, 0x1000
+    
+    int 0x13 
+
+    mov byte[es:bx], 0xF8
+    add bx, 1
+    mov byte[es:bx], 0xFF
+    add bx, 1
+    mov byte[es:bx], 0xFF
+    add bx, 1
+    mov byte[es:bx], 0xFF
+
+    mov ah, 0x03
+    mov bx, 0x1000
+    int 0x13
+
+    popa
+    ret
+
+INIT_FAT:
+    mov ax, [BPB_RsvdSecCnt]
+    mov [FatStartSector], ax
+
+    mov ax, [BPB_FATSz16]
+    movzx cx, byte [BPB_NumFATs]
+    mul cx
+    mov [FatSectors], ax
+
+    mov ax, [FatSectors]
+    add ax, [FatStartSector]
+    mov [RootDirStartSector], ax
+
+    mov ax, [BPB_RootEntCnt]
+    mov cx, 32
+    mul cx
+    add ax, [BPB_BytsPerSec]
+    sub ax, 1
+    mov cx, [BPB_BytsPerSec]
+    div cx
+    mov [RootDirSectors], ax
+
+    mov ax, [RootDirStartSector]
+    add ax, [RootDirSectors]
+    mov [DataStartSector], ax
+
+    mov ax, [BPB_TotSec16]
+    sub ax, [DataStartSector]
+    mov [DataSectors], ax
+
+    ret
+
+LOAD_FAT:
+    mov si, lba
+    mov ah, 0x42
+    mov dl, 0x80
+
+    int 0x13
+
+    ret
+    
+LOAD_BOOT:
+    call LOAD_FAT
+    mov si, stage2_file
+    
+    mov bx, 0x7E00
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+    mov cx, 0
+
+.FIND_FILE_LOOP:
+    cmp cx, 512
+    je .NOT_FOUND
+    mov di, bx  
+    call compare_strings 
+    je .FOUND
+    
+    add cx, 32
+    add bx, 32
+
+    jmp .FIND_FILE_LOOP
+
+.NOT_FOUND:
+    ret
+
+.FOUND:
+    mov si, found
+    call print_string
+
+    ; Получаем сектор 
+    mov ax, word [FatStartSector]
+    add ax, word [FatSectors]
+    add ax, word [RootDirSectors]
+    push ax
+    mov ax, [es:bx + 0x1A]
+    sub ax, 0x002
+    mov cx, word [BPB_SecPerClus]
+    mul cx
+    pop bx
+
+    add ax, bx              ; Вот сектор
+
+    add ax, [BPB_RsvdSecCnt]
+    mov cx, 2
+    push ax
+    
+    mov ax, [BPB_FATSz16]
+    mul cx
+    mov bx, ax
+    
+    pop ax
+
+    
+    mov cx, 0x8000              ; Грузим на 0x0000:0x8000
+    mov bx, 0x0000          
+    mov dx, 0x0006              ; 6 Секторов
+    mov si, 0x0000 
+    mov ax, 153 
+    mov word [lba+8],  ax
+    mov word [lba+10], si
+    mov word [lba+6],  bx
+    mov word [lba+4],  cx
+    mov word [lba+2],  dx
+
+    mov si, lba
+    mov ah, 0x42
+    mov dl, 0x80
+    int 0x13 
+
+    jmp 0x0000:0x8000
+
+    ret
+
+compare_strings:
+    pusha
+    xor cx, cx
+
+.next_char: 
+    lodsb
+    
+    cmp cx, 11
+    je .equal
+    
+    cmp al, [di] 
+    jne .not_equal
+
+    cmp al, 0
+    je .equal
+   
+    inc di
+    inc cx
+    
+    jmp .next_char
+
+.not_equal:
+    popa
+    ret
+
+.equal: 
+    popa
+    ret
+    
+lba:
+    db 0x10
+    db 0x00
+    dw 1
+    dw 0x7E00
+    dw 0x0000
+    dq 129
+
+RootDirStartSector: dw 0
+RootDirSectors:     dw 0 
+FatSectors:         dw 0
+FatStartSector:     dw 0
+DataStartSector:    dw 0
+DataSectors:        dw 0
+found: db "found",0
+not_found: db "error: BOOTLOADER not found",0
+stage2_file: db "BOOT    BIN",0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
