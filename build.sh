15211fff8eb55cb1f06ae9a0c38caa1f9f47f9f8
Zhoski <alekkseev.a@mail.ru>
2026-09-14 18:44:52 +0500

    Тестовый запуск на реальном железе

--- build.sh
+++ build.sh
@@ -97,19 +97,19 @@ make clean
 cd ..
 cd ..
 
-./utilities/afsm -c test4.img
-./utilities/afsm -boot test4.img boot.bin
-./utilities/afsm -push test4.img rootFS/stage2.bin
-./utilities/afsm -push test4.img rootFS/kernel.bin
-./utilities/afsm -push test4.img rootFS/shell.bin
-./utilities/afsm -push test4.img rootFS/LICENSE.txt
-./utilities/afsm -push test4.img rootFS/hello.bin
-./utilities/afsm -push test4.img rootFS/init.bin
-./utilities/afsm -push test4.img rootFS/init.cfg
-./utilities/afsm -push test4.img rootFS/setup.bin
-./utilities/afsm -push test4.img rootFS/notepad.bin
+./utilities/afsm -c test5.img
+./utilities/afsm -boot test5.img boot.bin
+./utilities/afsm -push test5.img rootFS/stage2.bin
+./utilities/afsm -push test5.img rootFS/kernel.bin
+./utilities/afsm -push test5.img rootFS/shell.bin
+./utilities/afsm -push test5.img rootFS/LICENSE.txt
+./utilities/afsm -push test5.img rootFS/hello.bin
+./utilities/afsm -push test5.img rootFS/init.bin
+./utilities/afsm -push test5.img rootFS/init.cfg
+./utilities/afsm -push test5.img rootFS/setup.bin
+./utilities/afsm -push test5.img rootFS/notepad.bin
 
-qemu-system-i386 -drive format=raw,file=test4.img -m 16M -icount shift=6,sleep=off -rtc clock=vm -no-reboot 
+qemu-system-i386 -drive format=raw,file=test5.img -m 16M -icount shift=6,sleep=off -rtc clock=vm -no-reboot 
 
 rm binaries/boot.bin
 rm binaries/stage2.bin
