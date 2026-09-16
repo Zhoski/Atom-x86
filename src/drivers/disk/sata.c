f44cbc321fdf2f67ff41479d7cf184222205829d
Zhoski <alekkseev.a@mail.ru>
2026-09-17 00:32:15 +0500

    Тестовый запуск на реальном железе

--- src/drivers/disk/sata.c
+++ src/drivers/disk/sata.c
@@ -2,9 +2,11 @@
 #include <drivers/video/video.h>
 
 struct HBA_regs* regs;
+struct HBA_port_regs* port_regs;
 
 U32 init_sata(U16 info[256]) {
     regs = (U32*)ahci_mem_base;
+    port_regs = (U32*)(ahci_mem_base + 0x100);
 
     if(regs->cap & 0x80000000) {
         video->write_string("Supports 64-bit Addressing: Yes\n");
@@ -35,6 +37,18 @@ U32 init_sata(U16 info[256]) {
     video->write_string("Port activity: ");
     video->write_int(port);
     video->write_string("\n");
+
+    video->write_string("Port status: ");
+
+    U8 port_status = (U8)(port_regs->ssts & 0xF);
+
+    if(port_status == 0x03) {
+        video->write_string("Device detected, connection established\n");
+    }else if(port_status == 0x01) {
+        video->write_string("Device detected, but connection not established\n");
+    }else {
+        video->write_string("Device not detected\n");
+    }
 
     return 2;
 }
--- src/drivers/include/drivers/disk/sata.h
+++ src/drivers/include/drivers/disk/sata.h
@@ -10,6 +10,28 @@ typedef struct HBA_regs {
     U32  pi;
 } __attribute__((packed));
 
+typedef struct HBA_port_regs {
+    U32      clb;
+    U32     clbu;
+    U32       fb;
+    U32      fbu;
+    U32       is;
+    U32       ie;
+    U32      cmd;
+    U32      rsv;
+    U32      tfd;
+    U32     ssts;
+    U32     sctl;
+    U32     serr;
+    U32     sact;
+    U32       ci;
+    U32     sntf;
+    U32      fbs;
+
+    U8 rsv1[0x7F - 0x44];
+
+} __attribute__((packed));
+
 typedef struct sata_disk {
     
 };
